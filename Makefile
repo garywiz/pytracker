e08c351144c1dee0afa235dfda35bd63d05d4634
Gary Wisniewski <garyw@blueseastech.com.au>
2013-05-08 06:55:51 +1000

    Added additional metadata for PyPi.

--- Makefile
+++ Makefile
@@ -14,6 +14,7 @@ install:
 clean:
 	python setup.py clean
 	test -e build && rm -rf build; true
+	test -e dist && rm -rf dist; true
 	test -e pytracker.egg-info && rm -rf pytracker.egg-info; true
 	test -e pytracker/trackable.so && rm pytracker/trackable.so; true
 	cd doc; make clean
--- setup.py
+++ setup.py
@@ -3,11 +3,31 @@ from setuptools import setup, Extension
 trackable = Extension('pytracker.trackable',
                       sources = ['pytracker/trackable.c'])
 
+with open('README.pypi') as file:
+    long_description = file.read()
+
 setup(
     name = "pytracker",
     version = '1.0',
-    description = 'Creates trackable objects for memory inventory purposes',
+    description = 'Object tracking classes for efficient inventory of allocated/freed objects',
+    long_description = long_description,
     ext_modules = [trackable],
     py_modules = ['pytracker/tracker', 'pytracker/__init__'],
     test_suite = "pyt_tests.tests.test_all",
+    license = "BSD 3",
+    author = "Gary Wisniewski",
+    author_email = "pypi-garywiz@gw.spidereye.com",
+    url = "http://github.com/garywiz/pytracker",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: C",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: System :: Monitoring",
+        ]
     )
